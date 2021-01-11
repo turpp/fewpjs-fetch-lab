abecb233b3fa89ad11a3e3c597f560121aed989c
turpp <white.tripp@icloud.com>
2021-01-11 17:42:47 -0600

    commit tree 30b548ff74cfda052cd25435a0e74c4bbc77a632

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books').then(function(respone){
+    return respone.json()
+  }).then(function(json){
+    renderBooks(json)
+  })
 }
 
 function renderBooks(books) {
